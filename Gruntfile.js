3d79c1eefbf7560722fbb7f8b2b75ca76d5e9695
Andrew Minion <andrew@andrewrminion.com>
2015-08-06 15:44:18 -0400

    add Gruntfile
    handle SCSS-to-css, post-process with autoprefixer and minify, minify SVGs, use browsersync for local development

---
+++ Gruntfile.js
@@ -0,0 +1,105 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    // Watch task config
+    watch: {
+        styles: {
+            files: "SCSS/*.scss",
+            tasks: ['sass', 'postcss'],
+        },
+        svg: {
+            files: "SVG/raw/*.svg",
+            tasks: ['svgmin', 'svgstore'],
+        },
+        icons: {
+            files: "images/*",
+            tasks: ['imageoptim'],
+        }
+    },
+    sass: {
+        dev: {
+            files: {
+                "style.css" : "SCSS/style.scss"
+            }
+        }
+    },
+    postcss: {
+        options: {
+            map: {
+                inline: false,
+            },
+
+            processors: [
+                require('pixrem')(), // add fallbacks for rem units
+                require('autoprefixer-core')({browsers: 'last 2 versions'}), // add vendor prefixes
+                require('cssnano')() // minify the result
+            ]
+        },
+        dist: {
+            src: 'style.css',
+        }
+    },
+    browserSync: {
+        dev: {
+            bsFiles: {
+                src : ['style.css', '*.php', '**/*.js'],
+            },
+            options: {
+                watchTask: true,
+                proxy: "incoming.ncll.dev",
+            }
+        }
+    },
+    svgmin: {
+        options: {
+            plugins: [
+                { removeViewBox: false },
+                { removeUselessStrokeAndFill: false },
+            ]
+        },
+        dist: {
+            files: [{
+                expand: true,
+                cwd: 'SVG/raw',
+                src: '*.svg',
+                dest: 'SVG/compressed',
+            }],
+        },
+    },
+    svgstore: {
+        options: {
+            prefix : 'icon-', // This will prefix each ID
+            svg: { // will add and overide the the default xmlns="http://www.w3.org/2000/svg" attribute to the resulting SVG
+                viewBox : '0 0 100 100',
+                xmlns: 'http://www.w3.org/2000/svg',
+            },
+        },
+        default: {
+            files: {
+                'images/icons.svg': ['SVG/compressed/*.svg'],
+            },
+        },
+    },
+    imageoptim: {
+//        svgIcons: {
+//            options: {
+//                imagealpha: true,
+//                jpegMini: false,
+//            },
+//            src: ['images/icons'],
+//        }
+    },
+  });
+
+    grunt.loadNpmTasks('grunt-imageoptim');
+    grunt.loadNpmTasks('grunt-svg2png');
+    grunt.loadNpmTasks('grunt-svgmin');
+    grunt.loadNpmTasks('grunt-svgstore');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.registerTask('default', [
+        'browserSync',
+        'watch',
+    ]);
+};
