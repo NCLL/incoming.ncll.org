e46e6f3aea9aa8f6cec4373eed8333dc5257ed6b
Andrew Minion <andrew@andrewrminion.com>
2015-08-11 16:17:55 -0400

    remove some unnecessary fields

--- process_form.php
+++ process_form.php
@@ -13,21 +13,15 @@ $nsc = startEtapestrySession();
 // Define Account
 $account = array();
 $account["nameFormat"] = 1;
-$account["name"] = "Larry Bird";
-$account["sortName"] = "Bird, Larry";
 $account["firstName"] = "Larry";
 $account["lastName"] = "Bird";
-$account["personaType"] = "Business";
+$account["personaType"] = "Personal";
 $account["address"] = "125 S. Pennsylvania Street";
 $account["city"] = "Indianapolis";
 $account["state"] = "IN";
 $account["postalCode"] = "46204";
 $account["country"] = "US";
 $account["email"] = "larry.bird@pacers.com";
-$account["webAddress"] = "www.pacers.com";
-$account["shortSalutation"] = "Larry";
-$account["longSalutation"] = "Mr. Bird";
-$account["envelopeSalutation"] = "Mr. Larry Bird";
 
 // Define Phone
 $phone = array();
@@ -36,27 +30,6 @@ $phone["number"] = "(317) 817-2500";
 
 // Add Phone to Account (optional)
 $account["phones"] = array($phone);
-
-// Define DefinedValue #1
-$dv1 = array();
-$dv1["fieldName"] = "Company";
-$dv1["value"] = "Indiana Pacers";
-
-// Add persona based DefinedValue #1 to Account (optional)
-$account["personaDefinedValues"] = array($dv1);
-
-// Define DefinedValue #2
-$dv2 = array();
-$dv2["fieldName"] = "Gender";
-$dv2["value"] = "Male";
-
-// Define DefinedValue #3
-$dv3 = array();
-$dv3["fieldName"] = "Date of Birth";
-$dv3["value"] = "12/7/1956";
-
-// Add account based DefinedValue #2 and #3 to Account (optional)
-$account["accountDefinedValues"] = array($dv2, $dv3);
 
 // Invoke addAccount method
 echo "Calling addAccount method...";
