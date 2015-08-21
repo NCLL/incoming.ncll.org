0112122761b706441d8cc63258eb6b6a785b9ae4
Andrew Minion <andrew@andrewrminion.com>
2015-08-21 10:24:13 -0400

    animate form height on button clicks

--- js/main.js
+++ js/main.js
@@ -3,13 +3,21 @@ $( document ).ready( function() {
         event.preventDefault();
     });
     $( 'form' ).on( 'click', '.next', function( event ) {
-        $( this ).parent( 'fieldset' ).fadeOut(400, function() {
-            $(this).next().fadeIn();
+        var $formHeight = $( '.signup' ).height();
+        var $thisHeight= $( this ).parent( 'fieldset' ).height();
+        var $nextHeight = $( this ).parent( 'fieldset' ).next().actual( 'height' );
+        $( '.signup' ).animate( { height: $formHeight - $thisHeight + $nextHeight } );
+        $( this ).parent( 'fieldset' ).fadeOut(300, function() {
+            $( this ).next().fadeIn();
         });
     });
     $( 'form' ).on( 'click', '.back', function( event ) {
-        $( this ).parent( 'fieldset' ).fadeOut(400, function() {
-            $(this).prev().fadeIn();
+        var $formHeight = $( '.signup' ).height();
+        var $thisHeight= $( this ).parent( 'fieldset' ).height();
+        var $prevHeight = $( this ).parent( 'fieldset' ).prev().actual( 'height' );
+        $( '.signup' ).animate( { height: $formHeight - $thisHeight + $prevHeight } );
+        $( this ).parent( 'fieldset' ).fadeOut(300, function() {
+            $( this ).prev().fadeIn();
         });
     });
 });
